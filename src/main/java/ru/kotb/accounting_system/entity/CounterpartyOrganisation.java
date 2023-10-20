5176846a0025b75098f8b28330dc955714439a53
kotb <kotyachy.andegraund@gmail.com>
2023-10-20 22:58:29 +0300

    Add the counterparty organization Entity

---
+++ src/main/java/ru/kotb/accounting_system/entity/CounterpartyOrganisation.java
@@ -0,0 +1,89 @@
+package ru.kotb.accounting_system.entity;
+
+
+import javax.persistence.*;
+
+/**
+ * The class that describes the contracting company.
+ */
+@Entity
+@Table(name = "counterparty_organisations")
+public class CounterpartyOrganisation {
+
+    /**
+     * The entity primary key.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    /**
+     * The counterparty company name.
+     */
+    @Column(name = "name")
+    private String name;
+
+    /**
+     * The company address.
+     */
+    @Column(name = "address")
+    private String address;
+
+    /**
+     * The unique code that regulates the accounting of taxpayers.
+     */
+    @Column(name = "TIN")
+    private String TIN;
+
+    public CounterpartyOrganisation() {
+    }
+
+    public CounterpartyOrganisation(String name, String address, String TIN) {
+        this.name = name;
+        this.address = address;
+        this.TIN = TIN;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTIN() {
+        return TIN;
+    }
+
+    public void setTIN(String TIN) {
+        this.TIN = TIN;
+    }
+
+    @Override
+    public String toString() {
+        return "Counterparty{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", TIN='" + TIN + '\'' +
+                '}';
+    }
+}
