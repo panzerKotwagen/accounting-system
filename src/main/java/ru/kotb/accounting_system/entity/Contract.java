aad276fa826105d9a0c44ae785cccd8611c2c702
kotb <kotyachy.andegraund@gmail.com>
2023-10-20 23:33:51 +0300

    Add the contract Entity

---
+++ src/main/java/ru/kotb/accounting_system/entity/Contract.java
@@ -0,0 +1,77 @@
+package ru.kotb.accounting_system.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * The class that describes the contract that is concluded between
+ * the company and the customer.
+ */
+@Entity
+@Table(name = "contracts")
+public class Contract {
+
+    /**
+     * The entity primary key.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    /**
+     * The contract name.
+     */
+    @Column(name = "name")
+    private String name;
+
+    /**
+     * The type of the contract.
+     */
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="contract_type_id")
+    private ContractType contractType;
+
+    /**
+     * Expected date when does the contract enter into force.
+     */
+    @Column(name = "planned_start_date")
+    private String plannedStartDate;
+
+    /**
+     * Actual date when does the contract enter into force.
+     */
+    @Column(name = "actual_start_date")
+    private String actualStartDate;
+
+    /**
+     * Expected date when does the contract end.
+     */
+    @Column(name = "planned_end_date")
+    private String plannedEndDate;
+
+
+    /**
+     * Actual date when does the contract end.
+     */
+    @Column(name = "actual_end_date")
+    private String actualEndDate;
+
+    /**
+     * The contract amount.
+     */
+    @Column(name = "amount")
+    private int amount;
+
+    /**
+     * The stages of the contract.
+     */
+//    @OneToMany(mappedBy = cascade = CascadeType.ALL)
+//    private List<ContractStage> contractStages;
+
+    /**
+     * The contract with the counterparty that included into the contract.
+     */
+//    @Column(name = "")
+//    private List<CounterpartyContract> counterpartyContracts;
+}
