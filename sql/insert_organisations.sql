d956461613ea979d252afc046eccf489f7eb2a7e
kotb <kotyachy.andegraund@gmail.com>
2023-10-22 14:17:19 +0300

    Add SQL script for inserting organisations

---
+++ sql/insert_organisations.sql
@@ -0,0 +1,5 @@
+insert into accounting_system.organisations (name, address, TIN)
+values
+    ('CAT', 'Chlumínská 12/721 904 18 Český Těšín Česká republika', '1234657891'),
+    ('Company', 'Via Jelena 8 Appartamento 25 San Bibiana lido, 36610 Ragusa (BA)', '3214657891'),
+    ('RTY', 'Baranjska ulica 76, 58011 Mali Lošinj', '9874657891');
