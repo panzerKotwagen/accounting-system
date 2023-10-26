1531ef58253992442dc3cf06cbdaad7dbab1bf72
kotb <kotyachy.andegraund@gmail.com>
2023-10-26 10:00:23 +0300

    Add organisation contract DAO

---
+++ src/main/java/ru/kotb/accounting_system/dao/OrganisationContractDAO.java
@@ -0,0 +1,7 @@
+package ru.kotb.accounting_system.dao;
+
+
+import ru.kotb.accounting_system.entity.OrganisationContract;
+
+public interface OrganisationContractDAO extends CommonDAO<OrganisationContract>{
+}
---
+++ src/main/java/ru/kotb/accounting_system/dao/impl/OrganisationContractDAOImpl.java
@@ -0,0 +1,24 @@
+package ru.kotb.accounting_system.dao.impl;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.kotb.accounting_system.dao.OrganisationContractDAO;
+import ru.kotb.accounting_system.entity.OrganisationContract;
+
+
+@Repository
+public class OrganisationContractDAOImpl extends AbstractDAO<OrganisationContract>
+        implements OrganisationContractDAO {
+
+    /**
+     * Creates the component and binds it with the sessionFactory
+     * object.
+     *
+     * @param sessionFactory the SessionFactory object
+     */
+    @Autowired
+    public OrganisationContractDAOImpl(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+}
