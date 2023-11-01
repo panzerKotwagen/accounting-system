f6a966700a9d6bba9e83b85e0e51abd4a35a86b3
kotb <kotyachy.andegraund@gmail.com>
2023-11-01 17:38:58 +0300

    Add role entity

---
+++ src/main/java/ru/kotb/accounting_system/entity/Role.java
@@ -0,0 +1,43 @@
+package ru.kotb.accounting_system.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+
+/**
+ * The entity class that describes the user roles to access the API.
+ */
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role extends AbstractEntity implements GrantedAuthority {
+
+    /**
+     * The name of the role.
+     */
+    @Column(name = "authority", nullable = false, unique = true)
+    private String name;
+
+    /**
+     * The users with this role.
+     */
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    /**
+     * Returns the role name.
+     *
+     * @return the role name
+     */
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+}
