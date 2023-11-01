55fed45ef6c5b25edf24421a0e841810b6f50d20
kotb <kotyachy.andegraund@gmail.com>
2023-11-01 16:22:24 +0300

    Add find user by login

--- src/main/java/ru/kotb/accounting_system/dao/UserDAO.java
+++ src/main/java/ru/kotb/accounting_system/dao/UserDAO.java
@@ -7,4 +7,12 @@ import ru.kotb.accounting_system.entity.User;
  * The DAO interface used to access the table "users".
  */
 public interface UserDAO extends CommonDAO<User> {
+
+    /**
+     * Returns the user with the specified login.
+     *
+     * @param userLogin user login
+     * @return the user with the specified login or null
+     */
+    User findByLogin(String userLogin);
 }
--- src/main/java/ru/kotb/accounting_system/dao/impl/UserDAOImpl.java
+++ src/main/java/ru/kotb/accounting_system/dao/impl/UserDAOImpl.java
@@ -1,12 +1,12 @@
 package ru.kotb.accounting_system.dao.impl;
 
-
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.kotb.accounting_system.dao.UserDAO;
 import ru.kotb.accounting_system.entity.User;
-
-import javax.persistence.EntityManager;
 
 
 /**
@@ -19,10 +19,27 @@ public class UserDAOImpl extends AbstractDAO<User> implements UserDAO {
      * Creates the component and binds it with the sessionFactory
      * object.
      *
-     * @param sessionFactory the EntityManager object
+     * @param sessionFactory the SessionFactory object
      */
     @Autowired
-    public UserDAOImpl(EntityManager sessionFactory) {
+    public UserDAOImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
+    }
+
+    /**
+     * Returns the user with the specified login.
+     *
+     * @param userLogin user login
+     * @return the user with the specified login or null
+     */
+    @Override
+    public User findByLogin(String userLogin) {
+        Session session = sessionFactory.getCurrentSession();
+
+        Query<User> query = session.createQuery("from User " +
+                "where login = :userLogin", User.class);
+        query.setParameter("userLogin", userLogin);
+
+        return query.uniqueResult();
     }
 }
