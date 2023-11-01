5c363854547f3d0b8437e11b671229ec87f606b9
kotb <kotyachy.andegraund@gmail.com>
2023-11-01 17:52:38 +0300

    Implemented UserDetails in user entity

--- src/main/java/ru/kotb/accounting_system/entity/User.java
+++ src/main/java/ru/kotb/accounting_system/entity/User.java
@@ -1,25 +1,35 @@
 package ru.kotb.accounting_system.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.Collection;
+import java.util.Set;
 
 
 /**
- * The Entity class that describes the customer, which is linked
+ * The entity class that describes the customer, which is linked
  * with the MySQL table "users".
  */
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class User extends AbstractEntity {
+public class User extends AbstractEntity implements UserDetails {
 
     /**
      * The user full name.
@@ -57,11 +67,11 @@ public class User extends AbstractEntity {
      * The password must contain at least one uppercase, one lowercase
      * letter, one digit and consist of at least 8 characters.
      */
-    @Pattern(regexp = "^(?=.*[A-Z]).{1,}$",
+    @Pattern(regexp = "^(?=.*[A-Z]).+$",
             message = "Please write at least one uppercase letter")
-    @Pattern(regexp = "^(?=.*[0-9]).{1,}$",
+    @Pattern(regexp = "^(?=.*[0-9]).+$",
             message = "Please write at lest one digit")
-    @Pattern(regexp = "^(?=.*[a-z]).{1,}$",
+    @Pattern(regexp = "^(?=.*[a-z]).+$",
             message = "Please write at lest one lowercase letter")
     @Column(name = "password", unique = true)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
@@ -72,4 +82,59 @@ public class User extends AbstractEntity {
      */
     @Column(name = "end_date")
     private String endDate;
+
+    /**
+     * The roles of the user.
+     */
+    @Getter
+    @Transient
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    /**
+     * Returns the roles of the user.
+     *
+     * @return the roles of the user
+     */
+    @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    /**
+     * Return the user login.
+     *
+     * @return the user login
+     */
+    @Override
+    @JsonIgnore
+    public String getUsername() {
+        return login;
+    }
+
+    //TODO: add comments
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return true;
+    }
 }
