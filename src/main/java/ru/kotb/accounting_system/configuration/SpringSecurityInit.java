b19b0484ee640caec16ed793242a7b0685f6ad66
kotb <kotyachy.andegraund@gmail.com>
2023-11-05 19:48:07 +0300

    Add SecurityWebApplicationInitializer

---
+++ src/main/java/ru/kotb/accounting_system/configuration/SpringSecurityInit.java
@@ -0,0 +1,7 @@
+package ru.kotb.accounting_system.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+public class SpringSecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
