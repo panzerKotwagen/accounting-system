bc703b88c8639b72f4909685096b56ea786c4d46
kotb <kotyachy.andegraund@gmail.com>
2023-11-06 10:11:40 +0300

    Fix: cannot decide whether pattern is MVC

--- src/main/java/ru/kotb/accounting_system/configuration/SecurityConfig.java
+++ src/main/java/ru/kotb/accounting_system/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.kotb.accounting_system.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 
 @EnableWebSecurity
@@ -34,10 +37,17 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            HandlerMappingIntrospector introspector) throws Exception {
+
+        MvcRequestMatcher.Builder mvc = new MvcRequestMatcher.Builder(introspector);
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(mvc.pattern("/auth/**")).permitAll();
+                    auth.anyRequest().authenticated();
+                })
                 .httpBasic().and()
                 .build();
     }
