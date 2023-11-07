26d1f641d93e4c47a18f365bca7a3a414b3b5cb4
kotb <kotyachy.andegraund@gmail.com>
2023-11-07 13:58:27 +0300

    Remove useless file

--- src/main/java/ru/kotb/accounting_system/configuration/SpringSecurityInit.java
+++
@@ -1,7 +0,0 @@
-package ru.kotb.accounting_system.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-
-public class SpringSecurityInit extends AbstractSecurityWebApplicationInitializer {
-}
