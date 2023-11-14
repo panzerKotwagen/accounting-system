39ee2a62cbe13938c6714e873edc03b73b80993d
kotb <kotyachy.andegraund@gmail.com>
2023-11-14 14:12:08 +0300

    Add link to contract in counterparty contract response

--- src/main/java/ru/kotb/accounting_system/model_assembler/CounterpartyContractModelAssembler.java
+++ src/main/java/ru/kotb/accounting_system/model_assembler/CounterpartyContractModelAssembler.java
@@ -1,8 +1,15 @@
 package ru.kotb.accounting_system.model_assembler;
 
+import org.springframework.hateoas.EntityModel;
 import org.springframework.stereotype.Component;
+import ru.kotb.accounting_system.controller.ContractController;
 import ru.kotb.accounting_system.controller.CounterpartyContractController;
+import ru.kotb.accounting_system.controller.OrganisationController;
+import ru.kotb.accounting_system.entity.ContractStage;
 import ru.kotb.accounting_system.entity.CounterpartyContract;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 /**
  * The model assembler for the {@code CounterpartyContract} entity.
@@ -13,5 +20,23 @@ public class CounterpartyContractModelAssembler
 
     public CounterpartyContractModelAssembler() {
         super(CounterpartyContractController.class, "counterparty-contracts");
+    }
+
+    /**
+     * Wraps an entity into {@code EntityModel} with relevant links.
+     *
+     * @param entity the entity
+     * @return the entity model with the entity
+     */
+    @Override
+    public EntityModel<CounterpartyContract> toModel(CounterpartyContract entity) {
+        EntityModel<CounterpartyContract> entityModel = super.toModel(entity);
+        int associatedContractId = entity.getContract().getId();
+        int associatedOrgId = entity.getOrganisation().getId();
+        entityModel.add(
+                linkTo(methodOn(ContractController.class).get(associatedContractId)).withRel("contract"));
+        entityModel.add(
+                linkTo(methodOn(OrganisationController.class).get(associatedOrgId)).withRel("organisation"));
+        return entityModel;
     }
 }
