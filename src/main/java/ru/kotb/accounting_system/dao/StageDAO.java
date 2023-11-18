91144fc95fef900145a65620dfea2bb1477b526c
kotb <kotyachy.andegraund@gmail.com>
2023-11-18 13:45:22 +0300

    Add stage DAO

---
+++ src/main/java/ru/kotb/accounting_system/dao/StageDAO.java
@@ -0,0 +1,39 @@
+package ru.kotb.accounting_system.dao;
+
+import org.springframework.stereotype.Repository;
+import ru.kotb.accounting_system.entity.ContractStage;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+
+@Repository
+public class StageDAO extends AbstractDAO<ContractStage> {
+
+    /**
+     * Creates the component and binds it with the entityManager
+     * object.
+     *
+     * @param entityManager the EntityManager object
+     */
+    public StageDAO(EntityManager entityManager) {
+        super(entityManager);
+        setClass(ContractStage.class);
+    }
+
+    /**
+     * Returns list of the stages of the specified contract.
+     *
+     * @param contractId the id of the contract
+     * @return list of the stages of the specified contract
+     */
+    public List<ContractStage> findAllByContractId(int contractId) {
+        Query query = entityManager.createQuery(" FROM ContractStage " +
+                "where contract.id = :contractId");
+
+        query.setParameter("contractId", contractId);
+
+        return (List<ContractStage>) query.getResultList();
+    }
+}
