058757547eadd520bb4e38d606cdb44a574c3eb0
kotb <kotyachy.andegraund@gmail.com>
2023-11-21 17:15:41 +0300

    Add sign in method into auth controller

--- src/main/java/ru/kotb/accounting_system/controller/AuthenticationController.java
+++ src/main/java/ru/kotb/accounting_system/controller/AuthenticationController.java
@@ -1,6 +1,12 @@
 package ru.kotb.accounting_system.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +24,9 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
     public AuthenticationController(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
@@ -25,5 +34,15 @@ public class AuthenticationController {
     @PostMapping("/register")
     public User registerUser(@RequestBody RegistrationDTO body) {
         return authenticationService.registerUser(body);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<String> authenticateUser(@RequestBody RegistrationDTO body){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        body.getUsername(), body.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("User signed-in successfully!", HttpStatus.OK);
     }
 }
