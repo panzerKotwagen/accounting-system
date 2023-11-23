bdd20c109de3a894c4b41978defa38b304c32d9e
kotb <kotyachy.andegraund@gmail.com>
2023-11-23 17:29:58 +0300

    Add a container class for the Contract and CounterpartyContract entities

---
+++ src/main/java/ru/kotb/accounting_system/dto/ContractDTO.java
@@ -0,0 +1,48 @@
+package ru.kotb.accounting_system.dto;
+
+import ru.kotb.accounting_system.entity.AbstractContract;
+import ru.kotb.accounting_system.entity.Contract;
+import ru.kotb.accounting_system.entity.CounterpartyContract;
+
+public class ContractDTO extends AbstractContract {
+
+    /**
+     * The contract type.
+     */
+    private Type type;
+
+    /**
+     * The name of the contract that the contract with counterparties
+     * is associated with.
+     */
+    private String mainContractName;
+
+    /**
+     * Constructs DTO from the {@code Contract} entity.
+     */
+    public ContractDTO(Contract contract) {
+        type = Type.MAIN_CONTRACT;
+        setName(contract.getName());
+        setContractType(contract.getContractType());
+        setAmount(contract.getAmount());
+        setActualStartDate(contract.getActualStartDate());
+        setActualEndDate(contract.getActualEndDate());
+        setPlannedStartDate(contract.getPlannedStartDate());
+        setPlannedEndDate(contract.getPlannedEndDate());
+    }
+
+    /**
+     * Constructs DTO from the {@code CounterpartyContract} entity.
+     */
+    public ContractDTO(CounterpartyContract contract) {
+        type = Type.COUNTERPARTY_CONTRACT;
+        setName(contract.getName());
+        setContractType(contract.getContractType());
+        setAmount(contract.getAmount());
+        setActualStartDate(contract.getActualStartDate());
+        setActualEndDate(contract.getActualEndDate());
+        setPlannedStartDate(contract.getPlannedStartDate());
+        setPlannedEndDate(contract.getPlannedEndDate());
+        mainContractName = contract.getContract().getName();
+    }
+}
---
+++ src/main/java/ru/kotb/accounting_system/dto/Type.java
@@ -0,0 +1,10 @@
+package ru.kotb.accounting_system.dto;
+
+
+/**
+ * The contract types for {@code ContractDTO}.
+ */
+public enum Type {
+    MAIN_CONTRACT,
+    COUNTERPARTY_CONTRACT
+}
