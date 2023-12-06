c33c67c1797b44378f80ae929f122ad91d01bffe
kotb <kotyachy.andegraund@gmail.com>
2023-12-06 17:56:31 +0300

    Add correct database dump

--- docker/db/data.sql
+++ docker/db/data.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
 --
--- Host: 127.0.0.1    Database: accounting_system
+-- Host: localhost    Database: accounting_system
 -- ------------------------------------------------------
--- Server version	8.0.34
+-- Server version	8.0.35
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -19,27 +19,21 @@
 -- Table structure for table `contract_stages`
 --
 
---
--- Create a database using `MYSQL_DATABASE` placeholder
---
-CREATE DATABASE IF NOT EXISTS `accounting_system`;
-USE `accounting_system`;
-
 DROP TABLE IF EXISTS `contract_stages`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `contract_stages` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `contract_id` int NOT NULL,
-  `amount` int NOT NULL,
-  `planned_start_date` date NOT NULL,
-  `planned_end_date` date NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `contract_id` int DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  `planned_start_date` date DEFAULT NULL,
+  `planned_end_date` date DEFAULT NULL,
   `actual_start_date` date DEFAULT NULL,
   `actual_end_date` date DEFAULT NULL,
-  `planned_material_cost` int NOT NULL,
+  `planned_material_cost` int DEFAULT NULL,
   `actual_material_cost` int DEFAULT NULL,
-  `planned_salary_cost` int NOT NULL,
+  `planned_salary_cost` int DEFAULT NULL,
   `actual_salary_cost` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `contract_id` (`contract_id`),
@@ -57,30 +51,6 @@ LOCK TABLES `contract_stages` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `contract_types`
---
-
-DROP TABLE IF EXISTS `contract_types`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `contract_types` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `contract_types`
---
-
-LOCK TABLES `contract_types` WRITE;
-/*!40000 ALTER TABLE `contract_types` DISABLE KEYS */;
-/*!40000 ALTER TABLE `contract_types` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `contracts`
 --
 
@@ -89,16 +59,14 @@ DROP TABLE IF EXISTS `contracts`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `contracts` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `contract_type_id` int NOT NULL,
-  `amount` int NOT NULL,
-  `planned_start_date` date NOT NULL,
-  `planned_end_date` date NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `contract_type` varchar(30) DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  `planned_start_date` date DEFAULT NULL,
+  `planned_end_date` date DEFAULT NULL,
   `actual_start_date` date DEFAULT NULL,
   `actual_end_date` date DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `contract_type_id` (`contract_type_id`),
-  CONSTRAINT `contracts_ibfk_1` FOREIGN KEY (`contract_type_id`) REFERENCES `contract_types` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -120,22 +88,20 @@ DROP TABLE IF EXISTS `organisation_contracts`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `organisation_contracts` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `contract_type_id` int NOT NULL,
-  `organisation_id` int NOT NULL,
-  `contract_id` int NOT NULL,
-  `amount` int NOT NULL,
-  `planned_start_date` date NOT NULL,
-  `planned_end_date` date NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `contract_type` varchar(30) DEFAULT NULL,
+  `organisation_id` int DEFAULT NULL,
+  `contract_id` int DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  `planned_start_date` date DEFAULT NULL,
+  `planned_end_date` date DEFAULT NULL,
   `actual_start_date` date DEFAULT NULL,
   `actual_end_date` date DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `contract_type_id` (`contract_type_id`),
   KEY `organisation_id` (`organisation_id`),
   KEY `contract_id` (`contract_id`),
-  CONSTRAINT `organisation_contracts_ibfk_1` FOREIGN KEY (`contract_type_id`) REFERENCES `contract_types` (`id`),
-  CONSTRAINT `organisation_contracts_ibfk_2` FOREIGN KEY (`organisation_id`) REFERENCES `organisations` (`id`),
-  CONSTRAINT `organisation_contracts_ibfk_3` FOREIGN KEY (`contract_id`) REFERENCES `contracts` (`id`)
+  CONSTRAINT `organisation_contracts_ibfk_1` FOREIGN KEY (`organisation_id`) REFERENCES `organisations` (`id`),
+  CONSTRAINT `organisation_contracts_ibfk_2` FOREIGN KEY (`contract_id`) REFERENCES `contracts` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -157,9 +123,9 @@ DROP TABLE IF EXISTS `organisations`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `organisations` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(150) NOT NULL,
-  `address` varchar(150) NOT NULL,
-  `TIN` varchar(10) NOT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  `address` varchar(150) DEFAULT NULL,
+  `TIN` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -221,7 +187,7 @@ CREATE TABLE `user_role_junction` (
 
 LOCK TABLES `user_role_junction` WRITE;
 /*!40000 ALTER TABLE `user_role_junction` DISABLE KEYS */;
-INSERT INTO `user_role_junction` VALUES (1,2),(2,1);
+INSERT INTO `user_role_junction` VALUES (1,2);
 /*!40000 ALTER TABLE `user_role_junction` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -234,14 +200,14 @@ DROP TABLE IF EXISTS `users`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `full_name` varchar(50) NOT NULL,
-  `login` varchar(20) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `full_name` varchar(50) DEFAULT NULL,
+  `login` varchar(20) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
   `end_date` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `login` (`login`),
   UNIQUE KEY `password` (`password`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -250,7 +216,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Viktor','admin','{bcrypt}$2a$10$l.tc59NJ9pfV4kvmfrFcEuJ.6X6qUqXd10xeoGtg2en3b84yuUOCy',NULL),(2,'Viktor Yablokov','Viktor','{bcrypt}$2a$10$H1uHFIawEQ4DjZ8aGR/4QeS4rtmS.tfrXZjrEM.CQaIWuqqov8PNe','2024-01-13');
+INSERT INTO `users` VALUES (1,'Viktor','admin','{bcrypt}$2a$10$IjsNWb0DS8C9ZquIKLh6fOXmcdd0x3BF1Qu0RF8DrkHtPEotVamyq',NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -263,4 +229,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-04 10:24:11
+-- Dump completed on 2023-12-06 17:54:24
