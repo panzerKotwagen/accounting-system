25389345d05ca4913a7b618a27a8e935e5c66e1e
kotb <kotyachy.andegraund@gmail.com>
2023-12-06 17:51:54 +0300

    Fix docker configuration and add bash scripts

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prepare Jar
+mvn clean
+mvn package
+
+cp -f ./target/accounting-system-0.0.1-SNAPSHOT.jar ./docker/app/accounting-system-0.0.1-SNAPSHOT.jar
+
+# Ensure, that docker-compose stopped
+docker-compose stop
+
+# Start new deployment
+docker-compose up --build -d
